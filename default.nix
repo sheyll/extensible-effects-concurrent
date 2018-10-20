b9e976d2eab32c93864adba22b15bee9ef0f5448
Sven Heyll <svh@posteo.de>
2018-10-20 05:23:25 +0200

    Improve nix expressions

--- default.nix
+++ default.nix
@@ -1,3 +1,25 @@
-{ nixpkgs ? import <nixos> {}, compiler ? "ghc843" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./extensible-effects-concurrent.nix {}
+{ q ? import <nixos> {  }
+, compiler ? "ghc843" }:
+let
+  nonHaskellBuildInputs =
+    with q.pkgs;
+    [cdrkit libvirt sudo openssh qemu rsync e2fsprogs xorriso];
 
+
+  cleanSrc = q.pkgs.lib.cleanSourceWith {
+    filter = (path: type:
+      let base = baseNameOf (toString path);
+      in !(q.pkgs.lib.hasPrefix ".ghc.environment." base) &&
+         !(q.pkgs.lib.hasSuffix ".nix" base)
+    );
+    src = q.pkgs.lib.cleanSource ./.;
+  };
+
+  pkg = q.pkgs.haskell.packages.${compiler}.callCabal2nix
+            "extensible-effects-concurrent" cleanSrc {};
+in
+  pkg.overrideAttrs(attrs: {
+      propagatedBuildInputs =
+           attrs.propagatedBuildInputs
+        ++ nonHaskellBuildInputs;
+  })
