3ca9c62639b882db1e6e27cdb9dac564fa6db71a
Sven Heyll <svh@posteo.de>
2019-04-06 04:42:38 +0200

    Fix nix expressions

--- default.nix
+++ default.nix
@@ -1,13 +1,2 @@
-{ lib, haskellPackages }:
-let
-  cleanSrc = lib.cleanSourceWith {
-    filter = (path: type:
-      let base = baseNameOf (toString path);
-      in !(lib.hasPrefix ".ghc.environment." base) &&
-         !(lib.hasSuffix ".nix" base)
-    );
-    src = lib.cleanSource ./.;
-  };
-
-  in haskellPackages.callCabal2nix
-       "extensible-effects-concurrent" cleanSrc {}
+{ pkgs ? (import <nixpkgs> {}) }:
+pkgs.callPackage ./extensible-effects-concurrent.nix {}
--- extensible-effects-concurrent.nix
+++ extensible-effects-concurrent.nix
@@ -1,2 +1,13 @@
-{ pkgs ? (import <nixpkgs> {}) }:
-pkgs.callPackage ./extensible-effects-concurrent.nix {}
+{ lib, haskellPackages }:
+let
+  cleanSrc = lib.cleanSourceWith {
+    filter = (path: type:
+      let base = baseNameOf (toString path);
+      in !(lib.hasPrefix ".ghc.environment." base) &&
+         !(lib.hasSuffix ".nix" base)
+    );
+    src = lib.cleanSource ./.;
+  };
+
+  in haskellPackages.callCabal2nix
+       "extensible-effects-concurrent" cleanSrc {}
