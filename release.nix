209e640c35a0f474f2f16c5c7175a4a1c6d162de
Sven Heyll <svh@posteo.de>
2020-11-27 19:43:20 +0100

    Remove obsolete files

--- release.nix
+++
@@ -1,9 +0,0 @@
-let
-  pkgs = import ./nix/pkgs.nix;
-  haskellPackages = pkgs.haskellPackages;
-  hsLib = pkgs.haskell.lib;
-in 
-    hsLib.dontHaddock (hsLib.dontCheck (import ./. 
-    { callCabal2nix = haskellPackages.callCabal2nix;
-      lib = pkgs.lib;
-    }))
